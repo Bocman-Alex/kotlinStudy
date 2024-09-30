4e8ec85cd145812902810d69954f7b92d1052396
bocman <145029546+Bocman-Alex@users.noreply.github.com>
2024-09-30 09:52:28 +0300

    creation

---
+++ app/src/main/java/presentation/MainActivity.kt
@@ -0,0 +1,15 @@
+package presentation
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.example.kotlinstudy.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+    }
+}
